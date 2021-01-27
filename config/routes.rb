f3336fc87a9ab7d722f580ec2b503a12dff1b0f8
Ionut Mladin <ionutm87@gmail.com>
2021-01-27 13:00:52 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Trestle::Engine.routes.draw do
   require "sidekiq/web"
 
-  mount Sidekiq::Web => "/sidekiq/web", constraints: Trestle::Auth::Constraint.new
-  get "/sidekiq/web", to: redirect("login", status: 302)
+  mount Sidekiq::Web => "/sidekiq/web"
 end
